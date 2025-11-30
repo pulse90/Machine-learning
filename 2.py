d9ce37a69f28292868269cc81dd9df642ced0883
Surya Pratap Singh <suryapt3456@gmail.com>
2025-11-30 21:54:23 +0530

    second chane

--- 2.py
+++ 2.py
@@ -1,5 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-xpt = np.array([0,10])
-ypt = np.array([0,120])
-plt.plot(xpt,ypt)
+
+# Data
+xpt = np.array([0, 10])
+ypt = np.array([0, 120])
+
+# Plot
+plt.figure()
+plt.plot(xpt, ypt)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Simple Plot")
+
+# Save to file
+plt.savefig("plot.png")   # <- added line to store the file
+
+# Optional: close the figure to free memory
+plt.close()
